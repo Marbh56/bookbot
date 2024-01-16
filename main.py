637c3b7d44c1929be1fd086ee12bdd348c5ff622
Marbh56 <nathan.a.sanders2@gmail.com>
2024-01-15 20:54:57 -0500

    Added code to sort and report how many letters are in a given text.

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 def main():
     #Your main program code here
     print("The program has started")
-    with open("books/Frankenstein") as f:
+    book_path = "books/Frankenstein"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_letters = count_letters(text)
+    report = generate_report(count_letters(text))    
     
-        file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+    print(f"{num_words} words found in the document")
+    #print(num_letters)
+    
 
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+        
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def lower_case(text):
+    return text.lower()
 
+def count_letters(text):
+    char_count_dictionary = {}
+    
+    text = lower_case(text)
+    for letter in text:
+        if letter.isalpha():
+            if letter in char_count_dictionary:
+                char_count_dictionary[letter] = char_count_dictionary[letter] + 1
+            elif letter not in char_count_dictionary:
+                char_count_dictionary[letter] = 1
+    return char_count_dictionary
+
+def generate_report(char_count_dictionary):
+    char_count_list = list(char_count_dictionary.items())
+    sorted_char_count_list = sorted(char_count_list, key=lambda x: x[1], reverse=True )
+    print("-- Begin report ---")
+    for char, count in sorted_char_count_list:
+        print(f"the '{char}' character was found {count} times")
+    print("--- End report ---")
+    
 if __name__ == "__main__":
     main()
